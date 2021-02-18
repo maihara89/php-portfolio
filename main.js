ba9326dc47c3f3f5a13882e2f45cf3a27e4b251d
maihara89 <75108175+maihara89@users.noreply.github.com>
2021-02-18 17:45:46 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -2,11 +2,23 @@
 
 {
     const open = document.getElementById('open');
+    const open2 = document.getElementById('open2');
+    const open3 = document.getElementById('open3');
     const close = document.getElementById('close');
     const modal = document.getElementById('modal');
     const mask = document.getElementById('mask');
 
     open.addEventListener('click',()=>{
+        modal.classList.remove('hidden');
+        mask.classList.remove('hidden');
+    });
+
+    open2.addEventListener('click',()=>{
+        modal.classList.remove('hidden');
+        mask.classList.remove('hidden');
+    });
+
+    open3.addEventListener('click',()=>{
         modal.classList.remove('hidden');
         mask.classList.remove('hidden');
     });
